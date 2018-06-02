c8315be4c51748230163f4b054cdc72580a6209f
Dhanup369 <dhanup369@gmail.com>
2018-06-01 21:52:26 -0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions illustrates cashing of an inverse matrix 
+##of the given matrix and thus demonstrates the concept of lexical scoping.
 
-## Write a short comment describing this function
-
+##The function "makeCacheMatrix"builds a set of functions 
+##and returns the functions within a list to the parent environment.
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) invmat <<- inv
+  getInverse <- function() invmat
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+##cacheSolve() is required to populate or retrieve 
+##the inverse matrix from an object of type makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getInverse()
+  if (!is.null(invmat)) {
+    message("getting cached data")
+    return(invmat)
+  }
+  mat <- x$get()
+  i <- solve(mat, ...)
+  x$setInverse(i)
+  i
 }
